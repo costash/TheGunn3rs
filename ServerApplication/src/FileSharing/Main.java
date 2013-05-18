ec2a0bef4de79550c84f0ec82aee177e6c057e11
Constantin Șerban-Rădoi <costashsrc@gmail.com>
2013-05-18 16:23:04 +0300

    Created structure

---
+++ ServerApplication/src/FileSharing/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package FileSharing;
+
+/**
+ * @author Costash
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
